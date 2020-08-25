079bddc3630804958ac0c85c691d03f6c989d685
Dairon Castro Casas <DaironCasas@sqdm.com>
2020-08-25 11:04:11 -0500

    Segundo commit

--- Program.cs
+++ Program.cs
@@ -7,16 +7,20 @@ namespace WindowsCMD
         static void Main(string[] args)
         {
             Metodos metodos = new Metodos();
+            int Tamanio = 100;
+            int[] Array = { 1, 4, -1, 3, 2, };
 
-            //int[] Array = { 1, 4, -1, 3, 2, };
+            // PRIMER EJERCICIO
+            Console.WriteLine("Ejercicio numero uno");
+            int posicion = metodos.ContarSaltos(Array);
+            Console.WriteLine("El -1 esta en el salto numero: " + posicion + "\n \n Ejercicio numero dos");
 
-            //int posicion = metodos.ContarSaltos(Array);
-            //Console.WriteLine("El -1 esta en el salto numero: " + posicion);
+            // SEGUNDO EJERCICIO
+            int numero = metodos.DistintoNumero(Array);
+            Console.WriteLine("El numero menor que no esta en el arrego es: " + numero + "\n \n Ejercicio numero tres");
 
-            //int numero = metodos.DistintoNumero(Array);
-            //Console.WriteLine("El numero menor que no esta en el arrego es: " + numero);
-
-            //metodos.Divisores(100);
+            // TERCER EJERCICIO
+            metodos.Divisores(Tamanio);
             
             Console.ReadKey();
         }
